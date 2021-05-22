284885b6599a748dfd15a15f7d10cd20c33c3d29
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-22 14:16:40 +0900

    ログのテキスト化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+    # パスワードの正規表現。半角英数字混合。
+
+    # 空では登録できない。
+    with_options presence :true do
+    validates :nickname
+    # validates :gender
+    # validates :age, numericality: true
+    end   
 end
