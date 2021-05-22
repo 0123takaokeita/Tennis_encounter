0401d820a0684f6a12be9797d7b3e8a59108de4e
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-22 10:30:10 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
+  resources :users, only: :show
   root 'plans#index'
-  get 'plans/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
