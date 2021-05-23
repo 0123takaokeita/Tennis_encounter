765da733e9c671955679a819308f59b0f9e09586
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-24 01:20:27 +0900

    テストデータ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
+    # password確認のための変数
+    word = 'aA1' + Faker::Internet.password
     
+    nickname              {Gimei.unique.name}
+    age                   {Faker::Date.between(from: '1950/1/1', to: Date.today.ago(16.years))}      
+    gender                {['男性','女性'][rand(2)]}
+    email                 {Faker::Internet.email}
+    password              {word}
+    password_confirmation {word}
+
+
+
   end
 end
