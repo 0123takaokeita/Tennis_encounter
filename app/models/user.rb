bd9b2b813a3d6fdf0ae3f6488b6d8ac406bc1ac4
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-29 22:31:52 +0900

    一意性のバリデーション追加（大文字区別）

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,10 @@ class User < ApplicationRecord
 
     # 空では登録できない。
     with_options presence: true do
-      validates :nickname
-      validates :gender_id
+      validates :nickname, uniqueness:{case_sensitive: false ,message: 'はすでに使用されています。'}
+      validates :gender_id, numericality: {other_than: 0, message: 'を選択してください。'}
       validates :age
     end   
 
-    validates :password, format: {with: password_regexp,message: '半角英数字を必ずふくめてください。'}
+    validates :password, format: {with: password_regexp,message: 'は半角英数字を必ずふくめてください。'}
 end
