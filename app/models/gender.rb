d27eaa07c9b0811dfc4237b7bf4fe51962d12e9e
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-29 22:30:53 +0900

    未選択を追加

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,6 +1,7 @@
 # in app/models/country.rb
 class Gender < ActiveHash::Base
   self.data = [
+    {:id => 0, :name => "---"},
     {:id => 1, :name => "男性"},
     {:id => 2, :name => "女性"},
     {:id => 3, :name => "その他"}
